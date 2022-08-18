0678efbda0df7f06714d989075c0cee021b2b740
Misha Rubanov <67386551+MishaRubanov@users.noreply.github.com>
2022-08-18 12:04:41 -0400

    circle target image

---
+++ circle_generator.py
@@ -0,0 +1,34 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Thu Aug 18 11:45:26 2022
+
+@author: rmish
+"""
+import numpy as np
+from scipy.special import comb
+import matplotlib.pyplot as plt
+import skimage
+
+# rr,cc = skimage.draw.circle_perimeter(5,5,3,shape=[11,11])
+# c_mat = np.zeros((11, 11), dtype=np.uint8)
+
+# c_mat[rr,cc] = 1
+# plt.figure()
+# plt.imshow(c_mat)
+h = 40
+w = 40
+c_mat = np.zeros((h, w,4), dtype=np.uint8)
+
+rr,cc = skimage.draw.disk((h//2,w//2),5)
+c_mat[rr,cc,:] = 1
+plt.figure()
+plt.imshow(c_mat[:,:,0])
+plt.savefig('cir.png')
+# f, axarr = plt.subplots(2,2)
+
+h = 72
+w = 72
+seed = np.zeros([h, w, 16], np.float32)
+seed[h//2, w//2, 3:] = 1.0
+# x = np.zeros([n, size, size, CHANNEL_N], np.float32)
+# x[:, size//2, size//2, 3:] = 1.0
